ae48c70434fa0ed103d6a6ed8a55e7a628fa5a5d
Bogdan <bogdanbagiag@gmail.com>
2026-03-20 08:42:47 +0200

    pws

--- middleware.ts
+++ middleware.ts
@@ -46,6 +46,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|auth|api|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|manifest.json|sw.js|workbox-.*|auth|api|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico|json)$).*)',
   ],
 }
